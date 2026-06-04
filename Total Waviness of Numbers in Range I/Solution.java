66359dd1320a4fbbb8bb0ddfe37f85e79cee8c5f
sawantyash07 <ysawant292@gmail.com>
2026-06-04 09:55:16 +0530

    Add LeetCode 3751 Total Waviness of Numbers in Range I solution

---
+++ Total Waviness of Numbers in Range I/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int totalWaviness(int num1, int num2) {
+        int total = 0;
+
+        for (int num = num1; num <= num2; num++) {
+            total += getWaviness(num);
+        }
+
+        return total;
+    }
+
+    private int getWaviness(int num) {
+        String s = String.valueOf(num);
+
+        if (s.length() < 3) return 0;
+
+        int waviness = 0;
+
+        for (int i = 1; i < s.length() - 1; i++) {
+            int prev = s.charAt(i - 1) - '0';
+            int curr = s.charAt(i) - '0';
+            int next = s.charAt(i + 1) - '0';
+
+            if ((curr > prev && curr > next) ||
+                (curr < prev && curr < next)) {
+                waviness++;
+            }
+        }
+
+        return waviness;
+    }
+}
